8aa0e4cee6842282be6c2f923283ad61e1185243
刘备 <admin@yitianyigexiangfa.com>
2024-06-06 10:39:12 +0800

    初始化项目

---
+++ src/main/java/com/yitianyigexiangfa/gitRemoteTest/GitRemoteTestApplication.java
@@ -0,0 +1,13 @@
+package com.yitianyigexiangfa.gitRemoteTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRemoteTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRemoteTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yitianyigexiangfa/gitRemoteTest/GitRemoteTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yitianyigexiangfa.gitRemoteTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRemoteTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
